d33d6dd44302ae3e2eae885d1995bcb877eba7d2
tcmofashi <mofashiforzbx@qq.com>
2025-06-01 17:51:49 +0800

    feat:更新版本为0.3.4

--- src/maim_message/__init__.py
+++ src/maim_message/__init__.py
@@ -1,6 +1,6 @@
 """Maim Message - A message handling library"""
 
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 
 from .api import MessageClient, MessageServer
 from .router import Router, RouteConfig, TargetConfig
