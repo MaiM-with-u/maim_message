cec8601d8ac7fef30f2400cd850e654bc0091ad6
tcmofashi <mofashiforzbx@qq.com>
2025-10-25 15:51:20 +0800

    tag:0.5.4

--- src/maim_message/__init__.py
+++ src/maim_message/__init__.py
@@ -1,6 +1,6 @@
 """Maim Message - A message handling library"""
 
-__version__ = "0.5.3"
+__version__ = "0.5.4"
 
 from .api import MessageClient, MessageServer
 from .router import Router, RouteConfig, TargetConfig
